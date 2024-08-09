9adc29b87e5bde05c19072efeb1c519753b3b6ee
Muchiri-cmd <davismuchiri21@gmail.com>
2024-08-09 22:47:11 +0300

    Debug build script

--- build.sh
+++ build.sh
@@ -8,9 +8,14 @@ pip install -r requirements.txt
 python manage.py collectstatic --no-input
 
 #Create superuser
-if [[ $CREATE_SUPERUSER ]];
-then
-  python3 manage.py createsuperuser --no-input
+echo "CREATE_SUPERUSER: $CREATE_SUPERUSER"
+echo "DJANGO_SUPERUSER_EMAIL: $DJANGO_SUPERUSER_EMAIL"
+echo "DJANGO_SUPERUSER_PASSWORD: $DJANGO_SUPERUSER_PASSWORD"
+echo "DJANGO_SUPERUSER_USERNAME: $DJANGO_SUPERUSER_USERNAME"
+
+if [[ "$CREATE_SUPERUSER" == "True" ]] || [[ "$CREATE_SUPERUSER" == "true" ]]; then
+  python manage.py createsuperuser --no-input
 fi
+
 # Apply any outstanding database migrations
 python manage.py migrate
