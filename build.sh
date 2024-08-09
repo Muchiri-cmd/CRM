1071f36aa78cd60f5ee36586bac8dd42a2879096
Muchiri-cmd <davismuchiri21@gmail.com>
2024-08-09 22:50:51 +0300

    Clean up build script   - Comment out script to avoid creating superuser on each build

--- build.sh
+++ build.sh
@@ -8,14 +8,9 @@ pip install -r requirements.txt
 python manage.py collectstatic --no-input
 
 #Create superuser
-echo "CREATE_SUPERUSER: $CREATE_SUPERUSER"
-echo "DJANGO_SUPERUSER_EMAIL: $DJANGO_SUPERUSER_EMAIL"
-echo "DJANGO_SUPERUSER_PASSWORD: $DJANGO_SUPERUSER_PASSWORD"
-echo "DJANGO_SUPERUSER_USERNAME: $DJANGO_SUPERUSER_USERNAME"
-
-if [[ "$CREATE_SUPERUSER" == "True" ]] || [[ "$CREATE_SUPERUSER" == "true" ]]; then
-  python manage.py createsuperuser --no-input
-fi
+# if [[ "$CREATE_SUPERUSER" == "True" ]] || [[ "$CREATE_SUPERUSER" == "true" ]]; then
+#   python manage.py createsuperuser --no-input
+# fi
 
 # Apply any outstanding database migrations
 python manage.py migrate
